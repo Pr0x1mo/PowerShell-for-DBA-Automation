e0bce627847c3071e271855d0298a7d612d5981d
Proximus Seraphim Dimitri Daviticus <Pr0x1mo@yahoo.com>
2024-07-16 11:08:42 -0500

    Delete PowerShell directory

--- PowerShell/SavePassword.ps1
+++
@@ -1,4 +0,0 @@
-# Save the password securely in a file
-$Password = "YourEmailPassword" | ConvertTo-SecureString -AsPlainText -Force
-$Password | ConvertFrom-SecureString | Out-File "C:\Secure\password.txt"
-
